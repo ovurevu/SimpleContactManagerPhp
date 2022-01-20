d080bd7b3a54f4047cdf50d7b2ce7fe4480f8c64
Emmanuel Ovurevu <emmanuel.ovurevu@gmail.com>
2022-01-20 23:04:48 +0100

    Stage 2. Refactor to use php templates in index.php

--- index.php
+++ index.php
@@ -1,76 +1,86 @@
-<?php session_start(); ?>
-<?php require 'layout/header.php' ?>
 <?php
+session_start();
 //include config file
 require_once ('helpers/db_config.php');
 
 // Attempt select query execution
 $sql = "SELECT * FROM contacts";
 
+//Variable to determine if there are contacts or not
+$zeroContacts = true;
+//Variable to determine if the connection was successful
+$dataLink = false;
+
 if($result = mysqli_query($link, $sql)){
+    $dataLink = true;
     if(mysqli_num_rows($result) > 0) {
-        echo '<main>';
-        echo '<div class="d-flex pb-3">';
-        echo '<a href="create.php" class="btn btn-primary px-4 ms-auto">Create New</a>';
-        echo '</div>';
-
-        if(isset($_SESSION['success'])) {
-            echo '<div class="alert alert-success text-center" role="alert">'.$_SESSION['success'].'</div>';
-            unset($_SESSION['success']);
-        }
-
-        if(isset($_SESSION['error'])) {
-            echo '<div class="alert alert-danger text-center" role="alert">'.$_SESSION['error'].'</div>';
-            unset($_SESSION['error']);
-        }
-            while ($row = mysqli_fetch_array($result)) {
-                echo '<div class="card mb-2">';
-                echo '<div class="card-body row">';
-                echo '<div class="col-10">';
-                echo '<h5 class="card-title">' . $row['first_name'] . ' ' . $row['last_name'] . '</h5>';
-                echo '<p class="card-text">' . $row['phone_number'] . '</p>';
-                echo '</div>';
-                echo '<div class="col-2">';
-                echo '<div class="dropdown float-end">';
-                echo '<a class="btn btn-secondary btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuContact1" data-bs-toggle="dropdown" aria-expanded="false">';
-                echo 'Actions';
-                echo '</a>';
-                echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">';
-                echo '<li>';
-                echo '<a class="dropdown-item" href="edit.php?id='.$row['id'].'">Edit</a>';
-                echo '</li>';
-                echo '<li><a class="dropdown-item" href="delete.php?id='.$row['id'].'">Delete</a></li>';
-                echo '</ul>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-
-            }
-        echo '</main>';
-        }else{
-            echo '<main class="pt-5 text-center">';
-            echo '<p class="lead">';
-            echo "Oops! You don't have any contacts yet. Create one to get started.";
-            echo '</p>';
-            echo '<p>';
-            echo '<a href="create.php" class="btn btn-lg btn-outline-primary">Create New Contact</a>';
-            echo '</p>';
-            echo '</main>';
-        }
-    // Free result set
-    mysqli_free_result($result);
-    } else{
-        echo '<main class="pt-5 text-center">';
-        echo '<p class="lead">';
-        echo "Oops! Something went wrong and the records could not be retrieved. Try again later.";
-        echo '</p>';
-        echo '</main>';
+        $zeroContacts = false;
     }
-
-
-// Close connection
-mysqli_close($link);
+}
 ?>
+
+<?php require 'layout/header.php' ?>
+<?php if ($dataLink): //If there is a connection ?>
+    <?php if (!$zeroContacts): //If the contacts are more than zero?>
+    <main>
+        <div class="d-flex pb-3">
+            <a href="create.php" class="btn btn-primary px-4 ms-auto">Create New</a>
+        </div>
+
+        <?php if(isset($_SESSION['success'])): ?>
+        <div class="alert alert-success text-center" role="alert"><?= $_SESSION['success'] ?></div>
+        <?php unset($_SESSION['success']); ?>
+        <?php endif; ?>
+
+        <?php if(isset($_SESSION['error'])): ?>
+            <div class="alert alert-danger text-center" role="alert"><?= $_SESSION['error'] ?></div>
+            <?php unset($_SESSION['error']); ?>
+        <?php endif; ?>
+        <?php while ($row = mysqli_fetch_array($result)): ?>
+        <div class="card mb-2">
+            <div class="card-body row">
+                <div class="col-10">
+                    <h5 class="card-title"><?= $row['first_name'] ?> <?= $row['last_name'] ?></h5>
+                    <p class="card-text"><?= $row['phone_number'] ?></p>
+                </div>
+                <div class="col-2">
+                    <div class="dropdown float-end">
+                        <a class="btn btn-secondary btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuContact1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Actions
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li>
+                                <a class="dropdown-item" href="edit.php?id=<?= $row['id'] ?>">Edit</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php endwhile; ?>
+    </main>
+    <?php else: //If there are zero contacts?>
+    <main class="pt-5 text-center">
+        <p class="lead">
+            Oops! You don't have any contacts yet. Create one to get started.
+        </p>
+        <p>
+            <a href="create.php" class="btn btn-lg btn-outline-primary">Create New Contact</a>
+        </p>
+    </main>
+    <?php endif; ?>
+    <?php mysqli_free_result($result); //free result set ?>
+<?php else: //If there was a bad db connection?>
+    <main class="pt-5 text-center">
+        <p class="lead">
+            Oops! Something went wrong and the records could not be retrieved. Try again later.
+        </p>
+    </main>
+<?php endif; ?>
+
 <?php require 'layout/footer.php' ?>
+<?php mysqli_close($link); //Close db connection?>
 
